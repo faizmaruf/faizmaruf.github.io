3e93a37ca2b7fa543776b57989bef7fb8fc8a138
faizmaruf <faiz.maruf8888@gmail.com>
2023-01-19 14:27:40 +0700

    update modal fiture

--- script.js
+++ script.js
@@ -45,7 +45,110 @@ document.addEventListener("DOMContentLoaded", function () { // On DOM Load initi
 // - when the web porto clicked
 // - show modal
 
-const soup = document.querySelector("#soup");
-soup.addEventListener('click', () => {
-    console.log('test')
-})
+
+const textModal = (label, description, image, technology) => {
+    const modalLabel = document.querySelector("#modalLabel");
+    // const modalBody = document.querySelector("#modalBody");
+    const modalDescription = document.querySelector("#modalDescription");
+    const modalLink = document.querySelector("#modalLink");
+    const modalImg = document.querySelector("#modalImg");
+    modalLabel.innerHTML = '';
+    modalLabel.appendChild(document.createTextNode(label));
+    // modalBody.innerHTML = '';
+    // modalBody.appendChild(document.createTextNode(body));
+    modalDescription.innerHTML = '';
+    modalDescription.appendChild(document.createTextNode(description));
+    modalImg.src = image;
+
+    list(technology)
+    // modalImg.appendChild(document.createTextNode(description));
+}
+
+const portofolio = [
+    {
+        id: '1',
+        label: 'Soup',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/spicysoup.jpeg',
+        technology: ['HTML', 'CSS', 'Bootstrap']
+    },
+    {
+        id: '2',
+        label: 'GuruBisa',
+        description: 'lorem ipsum color dumet sipp',
+        link: '#',
+        img: 'images/porto/gurubisa.jpeg',
+        technology: ['HTML', 'CSS', 'Bootstrap', 'CodeIgneter']
+    },
+    {
+        id: '3',
+        label: 'e-verification',
+        description: 'lorem ipsum color dumet',
+        link: '#',
+        img: 'images/porto/qrcode.jpeg',
+        technology: ['HTML', 'CSS', 'Bootstrap', 'JQuery']
+    }
+]
+
+
+const porto = portofolio.filter((portofoli) => portofoli.id == 1)[0];
+console.log(porto)
+
+//create list tech
+const list = (list) => {
+    const ul = document.querySelector("#modalList");
+    while (ul.hasChildNodes()) {
+        ul.removeChild(ul.firstChild);
+    }
+
+    for (var i = 0; i < list.length; i++) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(list[i]));
+        ul.appendChild(li);
+    }
+}
+
+
+// function for declar
+const portoAsli = document.querySelectorAll(".porto");
+portoAsli.forEach(occurence => {
+    occurence.addEventListener('click', (e) => {
+        const porto = portofolio.filter((portofoli) => portofoli.id == e.delegateTarget.id)[0];
+        // const porto = portofolio.filter((portofoli) => portofoli.id == 3);
+        // console.log(porto.technology);
+        ;
+        textModal(porto.label, porto.description, porto.img, porto.technology);
+        console.log(e.delegateTarget.id)
+        // console.log('A link was clicked');
+    });
+});
+
+
+
+
+// console.log(portoAsli[1]);
+// console.log(portoAsli.attributes);
+
+// const soup = document.querySelector(".porto");
+// console.log(soup.attributes.idporto.value);
+// portoAsli.addEventListener('click', (event) => {
+// textModal("ini judul suop", "ini body suop");
+// // console.log('test')
+// // console.log(porto);
+// console.log(event);
+// })
+// portoAsli.onclick = function (e) {
+//     textModal("ini judul suop", "ini body suop");
+//     // console.log('test')
+//     // console.log(porto);
+//     console.log(e);
+// };
+
+// const guruBisa = document.querySelector("#guruBisa");
+// guruBisa.addEventListener('click', () => {
+//     textModal("ini judul gurubisa", "ini body gurubisa")
+//     // console.log('test gurubisa')
+// })
+
+
