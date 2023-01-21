529f415bcb713c0e04641c689a23ee784dc221b7
faizmaruf <faiz.maruf8888@gmail.com>
2023-01-21 14:00:10 +0700

    add react.png and func for hide navbar

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// function for hide navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.transition = "top 0.2s"
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.transition = "top 0.2s"
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
+
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
