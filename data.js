c433a72d0187556c083d0af0c8430f586a7636aa
faizmaruf <faiz.maruf8888@gmail.com>
2023-02-09 17:44:40 +0700

    add images and update new user interface

---
+++ data.js
@@ -0,0 +1,76 @@
+const portofolio = [
+    {
+        id: '1',
+        label: 'Soup',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/spicysoup.jpeg',
+        technology: ['HTML', 'CSS', 'Bootstrap']
+    },
+    {
+        id: '2',
+        label: 'GuruBisa',
+        description: 'Guru Bisa is an ecourse that aims to improve the competence of teaching staff in the field of information technology',
+        link: '#',
+        img: 'images/porto/gurubisa.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter']
+    },
+    {
+        id: '3',
+        label: 'E-verification',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/qrcode.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
+    },
+    {
+        id: '4',
+        label: 'Web Kemala',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/kemala.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
+    },
+    {
+        id: '5',
+        label: 'Web Kemala',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/kemala.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
+    },
+    {
+        id: '6',
+        label: 'Web Kemala',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/kemala.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
+    },
+    {
+        id: '7',
+        label: 'Web Kemala',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/kemala.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
+    },
+    {
+        id: '8',
+        label: 'CMS Biofarma',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/cmsbio.png',
+        technology: ['HTML, CSS', 'Tailwind', 'React ts']
+    },
+    {
+        id: '9',
+        label: 'Web Surveyor PTSI BUMN',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
+        link: '#',
+        img: 'images/porto/websurveyor.png',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'Laravel', 'Dompdf', 'Leaflet Js']
+    }
+];
+
+export { portofolio };
--- script.js
+++ script.js
@@ -1,4 +1,83 @@
+// import { portofolio } from 'data.js';
+// console.log(portofolio);
 // function for hide navbar
+const portofolio = [
+    {
+        id: '1',
+        label: 'Soup',
+        description: 'Slicing UI from dribbble platform',
+        link: '#',
+        img: 'images/porto/spicysoup.jpeg',
+        technology: ['HTML', 'CSS', 'Bootstrap']
+    },
+    {
+        id: '2',
+        label: 'GuruBisa',
+        description: 'Guru Bisa is an ecourse that aims to improve the competence of teaching staff in the field of information technology',
+        link: 'https://github.com/faizmaruf/gurubisa',
+        img: 'images/porto/gurubisa.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter']
+    },
+    {
+        id: '3',
+        label: 'E-verification',
+        description: 'Voter document management system website to assist companies in managing and finding the required documents.',
+        link: '#',
+        img: 'images/porto/qrcode.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
+    },
+    {
+        id: '4',
+        label: 'Web Kemala',
+        description: 'Web Keluarga Mahasiswa Lampung Universitas Sriwijaya.',
+        link: 'https://github.com/faizmaruf/kemalaunsri',
+        img: 'images/porto/kemala.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
+    },
+    {
+        id: '5',
+        label: 'Sistem Pendukung Keputusan',
+        description: 'Decision Support System For Selecting The Best Futsal Player selection using Analytical Hierarchy Process and Technique for Order of Preference by Similarity to Ideal Solution Method.',
+        link: 'https://github.com/faizmaruf/spk',
+        img: 'images/porto/spk.jpeg',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter']
+    },
+    {
+        id: '6',
+        label: 'Lion Parcel',
+        description: 'Lionparcel Content Management System.',
+        link: '#',
+        img: 'images/porto/lionparcel.png',
+        technology: ['HTML, CSS', 'tailwind', 'Vue js']
+    },
+    {
+        id: '7',
+        label: 'Laurreta',
+        description: 'Lauretta Property Management App is a dashboard that will consume data from 1 shopping malls cameras and use it to generate dashboards for property management and retail analytics.',
+        link: '#',
+        img: 'images/porto/laurreta.png',
+        technology: ['HTML, CSS', 'tailwind', 'React Js', 'Express Js', 'Mysql', 'Echart 5']
+    },
+    {
+        id: '8',
+        label: 'CMS Biofarma',
+        description: 'Biofarma Content Management System.',
+        link: '#',
+        img: 'images/porto/cmsbio.png',
+        technology: ['HTML, CSS', 'Tailwind', 'React Ts']
+    },
+    {
+        id: '9',
+        label: 'Web Surveyor PTSI BUMN',
+        description: 'Web Surveyor is a web app to record the location of tower sites based on latitude and longitude which will be used as data for land acquisition.',
+        link: '#',
+        img: 'images/porto/websurveyor.png',
+        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'Laravel', 'Dompdf', 'Leaflet Js', 'Chart Js']
+    }
+];
+
+
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
@@ -62,40 +141,7 @@ document.addEventListener("DOMContentLoaded", function () { // On DOM Load initi
 // - show modal
 
 
-const portofolio = [
-    {
-        id: '1',
-        label: 'Soup',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
-        link: '#',
-        img: 'images/porto/spicysoup.jpeg',
-        technology: ['HTML', 'CSS', 'Bootstrap']
-    },
-    {
-        id: '2',
-        label: 'GuruBisa',
-        description: 'Guru Bisa is an ecourse that aims to improve the competence of teaching staff in the field of information technology',
-        link: '#',
-        img: 'images/porto/gurubisa.jpeg',
-        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter']
-    },
-    {
-        id: '3',
-        label: 'E-verification',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
-        link: '#',
-        img: 'images/porto/qrcode.jpeg',
-        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
-    },
-    {
-        id: '4',
-        label: 'Web Kemala',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis beatae, iusto vel quisquam temporibus repellendus dolores pariatur laborum porro saepe aut nesciunt ullam lo',
-        link: '#',
-        img: 'images/porto/kemala.jpeg',
-        technology: ['HTML, CSS', 'JQuery', 'Bootstrap', 'CodeIgneter', 'Dompdf']
-    }
-]
+
 
 //function for add text to each tag at modal
 const textModal = (label, description, image, technology, link) => {
@@ -142,7 +188,7 @@ const portoAsli = document.querySelectorAll(".porto");
 portoAsli.forEach(occurence => {
     occurence.addEventListener('click', (e) => {
         const porto = portofolio.filter((portofoli) => portofoli.id == e.delegateTarget.id)[0];
-        // console.log(porto);
+        console.log(porto);
         textModal(porto.label, porto.description, porto.img, porto.technology, porto.link);
         // console.log(e.delegateTarget.id)
     });
